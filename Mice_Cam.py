a2b0e202c400fbb8759a3c13bcc94a878ee4e8a8
Frohnberto <jonasfrohnhoefer@gmx.de>
2020-10-27 15:49:04 +0100

    Add files via upload

---
+++ Mice_Cam.py
@@ -0,0 +1,23 @@
+#from picamera import PiCamera
+from time import sleep
+import os
+
+ 
+newpath = r'\home\pi\Documents\TestFolder'
+if not os.path.exists(newpath):
+  os.makedirs(newpath)
+  print('Ordner erstellt!')
+else:
+    print('Ordner bereits vorhanden')
+
+
+
+
+
+
+
+
+
+
+          
+
