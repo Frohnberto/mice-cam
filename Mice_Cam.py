8f500f6beb46dc09527da81964cf3a43ffecdff8
Frohnberto <jonasfrohnhoefer@gmx.de>
2020-10-27 16:37:09 +0100

    Add files via upload

--- Mice_Cam.py
+++ Mice_Cam.py
@@ -3,7 +3,7 @@ from time import sleep
 import os
 
  
-newpath = r'\home\pi\Documents\TestFolder'
+newpath = r'/home/pi/Documents/TestFolder'
 if not os.path.exists(newpath):
   os.makedirs(newpath)
   print('Ordner erstellt!')
