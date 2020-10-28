af59d210163a1e2c54599677fa97090fb4ac70c9
Frohnberto <jonasfrohnhoefer@gmx.de>
2020-10-28 21:44:54 +0100

    replace pydrive with dropbox

--- Mice_Cam.py
+++ Mice_Cam.py
@@ -1,38 +1,26 @@
-# coding = utf-8
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+import dropbox
 from picamera import PiCamera
 from time import sleep
 import os
 
-#def serialUpload():
-#    folderName = 'Raspberry Pi'  # Please set the folder name.
-#    folders = drive.ListFile(
-#    {'q': "title='" + folderName + "' and mimeType='application/vnd.google-apps.folder' and trashed=false"}).GetList()
-#    for folder in folders:
-#        if folder['title'] == folderName:
-#            for i in range(2):
-#               filepath = r'' %i # Dateipfad für den Upload
-#                filename = 'testfile%i.jpg' %i
-#                file1 = drive.CreateFile({'title': file_name , 'parents': [{'id': folder['id']}]})  # Create GoogleDriveFile instance with title 'Hello.txt'.
-#                file1.SetContentFile(file_path) # Set content of the file from given string.
-#                file1.Upload()
-#    print('Upload Successful!')
-    
-def singleUpload(file_path, file_name):    
-    folderName = 'Raspberry Pi'  # Please set the folder name.
-    folders = drive.ListFile(
-    {'q': "title='" + folderName + "' and mimeType='application/vnd.google-apps.folder' and trashed=false"}).GetList()
-    for folder in folders:
-        if folder['title'] == folderName:
-            file1 = drive.CreateFile({'title': file_name , 'parents': [{'id': folder['id']}]})  # Create GoogleDriveFile instance with title 'Hello.txt'.
-            file1.SetContentFile(file_path) # Set content of the file from given string.
-            file1.Upload()
-    print('Upload Successful!')
 
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
-drive = GoogleDrive(gauth)
+
+dbx = dropbox.Dropbox('RJhMcFceZ30AAAAAAAAAAZCVRK0mdF-0g1CNt9y3gwqhk8wP95-rowt9GcjPgWNo')
+#dbx.users_get_current_account()
+#file_path = 
+#dbx.files_upload("testfile2", '\test2.odt')
+
+#for entry in dbx.files_list_folder('').entries:
+#    print(entry.name)
+
+
+
+
+
+    
+
+
+
     
 newpath = r'/home/pi/Documents/TestPictures'
 if not os.path.exists(newpath):
@@ -50,24 +38,18 @@ sleep(10)
 camera.awb_mode = 'auto'
 sleep(5)
 camera.capture('/media/pi/Documents/TestPictures/auto.jpg')
-filepath1 = r'/media/pi/Documents/TestPictures/auto.jpg' # Dateipfad für den Upload
-filename1 = 'auto.jpg'
 
 camera.awb_mode = 'shade'
 sleep(5)
 camera.capture('/media/pi/Documents/TestPictures/shade.jpg')
-filepath2 = r'/media/pi/Documents/TestPictures/shade.jpg' # Dateipfad für den Upload
-filename2 = 'shade.jpg'
 
 camera.awb_mode = 'cloudy'
 sleep(5)
 camera.capture('/media/pi/Documents/TestPictures/cloudy.jpg')
-filepath3 = r'/media/pi/Documents/TestPictures/cloudy.jpg' # Dateipfad für den Upload
-filename3 = 'cloudy.jpg'
 
-singleUpload(filepath1, filename1)
-singleUpload(filepath2, filename2)
-singleUpload(filepath3, filename3)
-
-
-
+with open('/media/pi/Documents/TestPictures/cloudy.jpg' , "rb") as f:
+    dbx.files_upload(f.read(), '/cloudy.jpg')
+with open('/media/pi/Documents/TestPictures/shade.jpg' , "rb") as f:
+    dbx.files_upload(f.read(), '/shade.jpg')
+with open('/media/pi/Documents/TestPictures/auto.jpg' , "rb") as f:
+    dbx.files_upload(f.read(), '/auto.jpg')
