36a5d11250ca604a83a843738bfb18cc6af8f2db
Frohnberto <jonasfrohnhoefer@gmx.de>
2020-10-28 12:46:03 +0100

    Update Mice_Cam.py

--- Mice_Cam.py
+++ Mice_Cam.py
@@ -11,7 +11,7 @@ import os
 #    for folder in folders:
 #        if folder['title'] == folderName:
 #            for i in range(2):
-#                file_path = r'' %i # Dateipfad für den Upload
+
 #                file_name = 'testfile%i.jpg' %i
 #                file1 = drive.CreateFile({'title': file_name , 'parents': [{'id': folder['id']}]})  # Create GoogleDriveFile instance with title 'Hello.txt'.
 #                file1.SetContentFile(file_path) # Set content of the file from given string.
